15615fa7ae872e89544bf11f474934bada3d5d12
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-06-12 05:11:38 +0100

    package settup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
+import { ViteTips } from 'vite-plugin-tips'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+	plugins: [
+		react(),
+		eslintPlugin(),
+		ViteTips()
+	]
 })
