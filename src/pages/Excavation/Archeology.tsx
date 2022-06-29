30b2e109d44ed3fa8307ef80d1f15efa0f3136a0
Ramleton <77259553+Ramleton@users.noreply.github.com>
2022-06-29 01:19:40 -0400

    Started archeology subskill of Excavation skill

--- src/pages/Excavation/Archeology.tsx
+++ src/pages/Excavation/Archeology.tsx
@@ -1,5 +1,5 @@
-import { Box, Title } from '@mantine/core'
-import SkillCards from 'components/SkillCards'
+import { Box, Grid, Title } from '@mantine/core'
+import Activity from 'components/Activity'
 import { FC } from 'react'
 
 const Archeology: FC = () => {
@@ -8,6 +8,36 @@ const Archeology: FC = () => {
 
 			<Title order={2}>Archeology</Title>
 
+			<Grid>
+				{
+					Object.values(archeologyData).map(({ name, image, xp, intervalTime, unlockLevel }) => {
+						return <Activity
+							key={name}
+							name={name}
+							unlocked={farmingLevel >= unlockLevel}
+							skill='agriculture'
+							subskill='farming'
+							image={image}
+							activity={{
+								activityName: `farming-${name}`,
+								intervalTime,
+								reward: {
+									addXp: {
+										amount: xp,
+										skill: 'excavation',
+										subskill: 'archeology'
+									},
+									addItem: {
+										materials: [
+											{ name: `${name.toLowerCase()}`, amount: [1, 3] }
+										]
+									}
+								}
+							}}
+						/>
+					})
+				}
+			</Grid>
 		</Box>
 	)
 }
