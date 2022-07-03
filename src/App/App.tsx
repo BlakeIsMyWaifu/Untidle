94b1d4a903e9ab69a39d5645af2b2de9f762ff6b
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-07-03 15:38:42 +0100

    💄 Custom scrollbar for main area

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { AppShell, MantineProvider } from '@mantine/core'
+import { AppShell, Box, MantineProvider, ScrollArea } from '@mantine/core'
 import { FC, Suspense } from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 import AppRoutes from 'routes/AppRoutes'
@@ -21,7 +21,7 @@ const App: FC = () => {
 				}}
 			>
 				<AppShell
-					padding='md'
+					padding={0}
 					navbar={<AppNavbar />}
 					styles={theme => ({
 						root: {
@@ -31,7 +31,14 @@ const App: FC = () => {
 					})}
 				>
 					<Suspense>
-						<AppRoutes />
+						<ScrollArea type='auto' style={{
+							height: '100vh',
+							boxSizing: 'border-box'
+						}}>
+							<Box m='md'>
+								<AppRoutes />
+							</Box>
+						</ScrollArea>
 
 						<ActivityLoop />
 					</Suspense>
